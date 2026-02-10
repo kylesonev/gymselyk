8337fe3c1126e9915cefd79579cf5bf1825243bf
Guilherme Rodrigues <mxtroy3@gmail.com>
2026-02-10 09:55:03 -0300

    ¨

---
+++ src/biblioteca.py
@@ -0,0 +1,116 @@
+TREINOS = {
+    "Full Body 3x": {
+        "Dia 1": [
+            {"ex": "Agachamento", "sets": "3x12"},
+            {"ex": "Supino Reto", "sets": "3x10"},
+            {"ex": "Remada Curvada", "sets": "3x12"},
+            {"ex": "Desenvolvimento", "sets": "2x12"},
+            {"ex": "Prancha", "sets": "3x30s"}
+        ],
+        "Dia 2": [
+            {"ex": "Leg Press", "sets": "3x12"},
+            {"ex": "Supino Inclinado", "sets": "3x10"},
+            {"ex": "Puxada Aberta", "sets": "3x12"},
+            {"ex": "Elevação Lateral", "sets": "2x12"},
+            {"ex": "Rosca Alternada", "sets": "3x10"},
+            {"ex": "Tríceps Francês", "sets": "3x10"},
+        ],
+        "Dia 3": [
+            {"ex": "Stiff", "sets": "3x12"},
+            {"ex": "Crucifixo Inclinado", "sets": "3x10"},
+            {"ex": "Remada Baixa", "sets": "3x12"},
+            {"ex": "Elevação Frontal", "sets": "2x12"},
+            {"ex": "Rosca Direta (Barra)", "sets": "3x10"},
+            {"ex": "Tríceps Polia", "sets": "3x10"},
+        ]
+    },
+    "Upper/Lower 2x": {
+        "Dia 1 (Superior)": [
+            {"ex": "Pulldown Unilateral", "sets": "4x10"},
+            {"ex": "T-Bar Row ", "sets": "4x10"},
+            {"ex": "Puxada Triângulo", "sets": "3x15"},
+            {"ex": "Supino Inclinado", "sets": "3x12"},
+            {"ex": "Voador", "sets": "3x12"},
+            {"ex": "Elevação Lateral", "sets": "3x12"},
+            {"ex": "Tríceps Polia", "sets": "3x12"},
+            {"ex": "Rosca Scott", "sets": "3x12"},
+        ],
+        "Dia 2 (Inferior)": [
+            {"ex": "Flexora Deitado", "sets": "4x12"},
+            {"ex": "Agachamento Pêndulo", "sets": "3x12"},
+            {"ex": "Cadeira Extensora", "sets": "3x15"},
+            {"ex": "Stiff", "sets": "4x20"},
+            {"ex": "Abdutor", "sets": "4x20"},
+            {"ex": "Adutor", "sets": "4x20"}
+        ],
+        "Dia 3 (Superior)": [
+            {"ex": "Puxada Aberta", "sets": "4x10"},
+            {"ex": "Remada Unilateral", "sets": "4x10"},
+            {"ex": "Supino Reto", "sets": "3x15"},
+            {"ex": "Crucifixo Inclinado", "sets": "3x12"},
+            {"ex": "Desenvolvimento", "sets": "3x12"},
+            {"ex": "Rosca Scott", "sets": "3x12"},
+            {"ex": "Tríceps Dips", "sets": "3x12"},
+            {"ex": "Lombar Banco", "sets": "3x12"},
+        ],
+        "Dia 4 (Inferior)": [
+            {"ex": "Flexora Sentado", "sets": "4x12"},
+            {"ex": "Agachamento", "sets": "3x12"},
+            {"ex": "Leg Press", "sets": "3x15"},
+            {"ex": "Elevação Pélvica", "sets": "4x20"},
+            {"ex": "Abdutor", "sets": "4x20"},
+            {"ex": "Adutor", "sets": "4x20"}
+        ],
+
+    },
+    "PPL 2x": {
+        "Dia 1 Push (Empurrar)": [
+            {"ex": "Supino Reto", "sets": "3x10"}, 
+            {"ex": "Crucifixo Reto", "sets": "3x10"}, 
+            {"ex": "Tríceps Pulley", "sets": "3x12"},
+            {"ex": "Skull Crusher", "sets": "3x10"}, 
+            {"ex": "Desenvolvimento", "sets": "3x10"},
+            {"ex": "Elevação Lateral", "sets": "3x10"},
+        ],
+        "Dia 2 Pull (Puxar)": [
+            {"ex": "Puxada Alta", "sets": "3x10"},
+            {"ex": "Remada Baixa", "sets": "3x10"},
+            {"ex": "Remada Curvada", "sets": "3x12"},
+            {"ex": "Rosca Alternada", "sets": "3x10"},
+            {"ex": "Rosca Scott", "sets": "3x10"},
+            {"ex": "Rosca Martelo", "sets": "3x12"},
+            
+        ],
+        "Dia 3 Legs (Pernas)": [
+            {"ex": "Agachamento", "sets": "3x10"},
+            {"ex": "Stiff", "sets": "3x12"},
+            {"ex": "Cadeira Extensora", "sets": "3x15"},
+            {"ex": "Elevação Pélvica", "sets": "4x10"},
+            {"ex": "Agachamento Búlgaro", "sets": "4x10"},            
+        ],
+        "Dia 4 Push (Empurrar)": [
+            {"ex": "Supino Inclinado", "sets": "3x10"}, 
+            {"ex": "Crossover Alto", "sets": "3x10"}, 
+            {"ex": "Tríceps Overhead", "sets": "3x12"},
+            {"ex": "Tríceps Francês", "sets": "3x10"}, 
+            {"ex": "Crucifixo Inverso", "sets": "3x10"},
+            {"ex": "Face Pull", "sets": "3x10"},
+        ],
+        "Dia 5 Pull (Puxar)": [
+            {"ex": "Puxada Alta", "sets": "3x10"},
+            {"ex": "Barra Fixa", "sets": "3x10"},
+            {"ex": "Remada Unilateral", "sets": "3x10"},
+            {"ex": "Rosca Alternada", "sets": "3x12"},
+            {"ex": "Rosca Martelo", "sets": "3x12"},
+            {"ex": "Rosca Direta (Barra)", "sets": "3x12"},
+        ],
+        "Dia 6 Legs (Pernas)": [
+            {"ex": "Abdutor", "sets": "3x10"},
+            {"ex": "Adutor", "sets": "3x12"},
+            {"ex": "Cadeira Flexora", "sets": "3x15"},
+            {"ex": "Leg Press", "sets": "4x10"},
+            {"ex": "Agachamento Sumô", "sets": "4x10"},
+            {"ex": "Elevação Panturrilha", "sets": "4x10"},   
+        ],
+    }
+}
